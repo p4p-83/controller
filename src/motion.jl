9992ed6e9de662cff88f1f1ea0477a6b6ca12cac
Sam Skinner <30613658+sskii@users.noreply.github.com>
2024-09-28 22:41:15 +1200

    serial port code and touch-off logic

--- src/motion.jl
+++ src/motion.jl
@@ -1,7 +1,15 @@
-using Base.Threads
+using Base.Threads, LibSerialPort
+import PiGPIO		# TODO prompt the user to run `sudo pigpiod` before running this  
+
+const coneLimitPin::Int = 0	# TODO
+
+# instances
+const piGpioInstance = PiGPIO.Pi()
+const gantryIo::LibSerialPort.SerialPort = open("/dev/ttyUSB0", 115200)
+const headIo::LibSerialPort.SerialPort = open("/dev/ttyACM0", 115200)
 
 ###################
-# MOVEMENT TYPES
+# TYPES TO REPRESENT MOVEMENTS
 
 @enum StartupManoeuvres begin
 	homeHead
@@ -40,6 +48,11 @@ function setMovement(m::Movement)
 	# TODO
 end
 
+function setHeadHoldingTorque()
+	global headIo
+	write(headIo, "\$1=255\r")
+end
+
 function awaitMovement()
 	lock(movementInProgressLock)
 	unlock(movementInProgressLock)
@@ -51,19 +64,19 @@ end
 
 # driver code to send the required gcode
 # everything should have been bounds checked and accounted for by this point — all points relative to home coordinates
-function sendMovementGcodes(; x=nothing, y=nothing, f1=600, u=nothing, v=nothing, r=nothing, f2=100)
+function sendMovementGcodes(; x=nothing, y=nothing, f1=600, u=nothing, v=nothing, r=nothing, th=1)
 
 	# create strings
 	gantryString = "G1"
 	if !isnothing(x) gantryString *= " X$x" end
 	if !isnothing(y) gantryString *= " Y$y" end
-	gantryString *= " F$f1\r"
+	gantryString *= " F$f1\n"	# TODO no support for feed rates on this controller
 	
 	headString = "G1"
 	if !isnothing(u) headString *= " X$u" end
 	if !isnothing(v) headString *= " Y$v" end
 	if !isnothing(r) headString *= " Z$r" end
-	headString *= " F$f2\r"
+	headString *= " F$th\r"		# TODO need to make sure this is in inverse time mode! (G93)
 
 	# dispatch
 	println(gantryString)
@@ -71,27 +84,56 @@ function sendMovementGcodes(; x=nothing, y=nothing, f1=600, u=nothing, v=nothing
 
 end
 
+# state
+datumFromHomeX::Float64 = 0.		# gantry, limit switch homing
+datumFromHomeY::Float64 = 0.		# gantry, limit switch homing
+headFromUprightU::Float64 = 0.		# head, operator homed TODO think this through a bit more
+headFromRetractedV::Float64 = 0.	# head, limit switch homed
+headTouchoffV::Float64 = 0.			# SOFT LIMIT used to track the last touch-off
+headRotation::Float64 = 0.			# no real home; just need the current pos.
+
+const head90degRotationU::Float64 = 5.5	# head, U axis movement corresponding to 90 degrees
+const headMaxExtensionV::Float64 = 1.8	# head, hard limit on head extension
+
 # internal routines to do these things (and block execution as the motion completes)
 # let Julia's dynamic dispatch paradigm do the hard work for us
 
+# incremental head lowering with polling of nozzle cone
 function touchoffHead()
-	# TODO
-	# incremental head lowering with polling of nozzle cone
+	global headFromRetractedV, headTouchoffV
+
+	extensionPerStep = 0.4
+	stepTime = 0.1 			# seconds
+
+	for v in range(headFromRetractedV, headMaxExtensionV, step=extensionPerStep)
+
+		# make a step
+		sendMovementGcodes(v=v, th=(60/stepTime))	# inverse time is in min⁻¹
+		sleep(stepTime)
+		headTouchoffV = v
+		if PiGPIO.read(piGpioInstance, coneLimitPin) break end	# TODO requires PiGPIO.set_mode(p::Pi, pin::Int, mode)
+
+	end
+
 end
 
 function executeHomeHead()
-	# TODO
-	# send homing command
-	# touch off nozzle cone for z height (can I make the assumption that the present gantry position is fine?)
+	global headIo
+	write(headIo, "G28\r") 	# G28 home	# TODO is this even possible — I don't have limits on the other axes! Do I need to do this from the Pi too?
+	write(headIo, "G93\r")	# G93 inverse time mode — touchoff needs this TODO can I find a more appropriate place for this?
+	touchoffHead()
+	# TODO review necessity of further moves
+	# sendMovementGcodes(u=????? no clue) TODO send help
 end
 
+# send homing command
 function executeHomeGantry()
-	# TODO
-	# send homing command
+	global gantryIo
+	write(gantryIo, "G28\n")
 end
 
 function executeMovement(m::StartupManoeuvres)
-	# two quite different options for a StartupManoeuvre — 
+	# two quite different options for a StartupManoeuvre — delegate to more specific functions
 	if m == homeHead return executeHomeHead()
 	elseif m == homeHead return executeHomeGantry()
 	else @error "Unimplemented"
